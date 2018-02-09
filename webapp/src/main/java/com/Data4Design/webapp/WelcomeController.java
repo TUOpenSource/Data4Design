2446c7cffefa9f656a504cb176c363d6efec508b
Z_Shaw <zns932@utulsa.edu>
2018-02-08 19:42:53 -0600

    Adding stuff

---
+++ webapp/src/main/java/com/Data4Design/webapp/WelcomeController.java
@@ -0,0 +1,24 @@
+package com.Data4Design.webapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
+
+@Controller
+public class WelcomeController {
+
+    // inject via application.properties
+    @Value("${welcome.message:test}")
+    private String message = "Hello World";
+
+    @RequestMapping("/")
+    public String welcome(Map<String, Object> model) {
+        model.put("message", this.message);
+        return "welcome";
+    }
+
+}
