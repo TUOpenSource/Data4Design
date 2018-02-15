2c28e1a5c0dd72340137c24158bb60a189d5d2fb
Z_Shaw <zns932@utulsa.edu>
2018-02-14 21:03:43 -0600

    Adding controllers back

---
+++ webapp/src/main/java/com/Data4Design/webapp/WebappApplication.java
@@ -0,0 +1,13 @@
+package com.Data4Design.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;  
+
+@SpringBootApplication
+public class WebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappApplication.class, args);
+	}
+}
---
+++ webapp/src/main/java/com/Data4Design/webapp/WelcomeController.java
@@ -0,0 +1,25 @@
+package com.Data4Design.webapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;  
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
+
+@RestController
+public class WelcomeController {
+
+    // inject via application.properties
+    @Value("${welcome.message:test}")
+    private String message = "Hello World";
+
+    @RequestMapping("/welcome")
+    public String welcome(Map<String, Object> model) {
+        model.put("message", this.message);
+        return "welcome";
+    }
+
+}
