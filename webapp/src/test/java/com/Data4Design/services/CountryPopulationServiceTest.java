bacbf37afc380bd5e8eb456d974ede3b0ee00470
bkshep <bshepherd027@gmail.com>
2018-03-08 19:26:47 -0600

    Added junit test for CountryPopulationService

---
+++ webapp/src/test/java/com/Data4Design/services/CountryPopulationServiceTest.java
@@ -0,0 +1,16 @@
+package com.Data4Design.services;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CountryPopulationServiceTest {
+
+	@Test
+	public void testGetPopulation() {
+		CountryPopulationService test = new CountryPopulationService();
+		test.getPopulation("usa");
+	}
+
+}
+
