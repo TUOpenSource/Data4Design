3998bdba8d2b994cdef0c34eb426b2e6765c6686
Brandon Stewart <bss162@utulsa.edu>
2018-05-05 22:23:16 -0500

    Updated FactBook URL

--- webapp/src/main/java/com/Data4Design/services/CIAFactBookService.java
+++ webapp/src/main/java/com/Data4Design/services/CIAFactBookService.java
@@ -17,7 +17,7 @@ public class CIAFactBookService implements ICIAFactBookService
         try
         {
             JSONParser parser = new JSONParser();
-            URL jsonFile = new URL("https://raw.githubusercontent.com/iancoleman/cia_world_factbook_api/master/data/2018-03-26_factbook.json");
+            URL jsonFile = new URL("https://raw.githubusercontent.com/iancoleman/cia_world_factbook_api/master/data/2018-04-30_factbook.json");
             BufferedReader reader = new BufferedReader(new InputStreamReader(jsonFile.openStream()));
 
             totalObject = (JSONObject) parser.parse(reader);
