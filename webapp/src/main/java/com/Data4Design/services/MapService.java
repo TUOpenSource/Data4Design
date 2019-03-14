bba0fc75cda8eae3d85f0fecee4db7bf82b5fd7e
JohnStorgion <johnStorgion@gmail.com>
2019-03-14 10:22:48 -0500

    Updated map to read API key through environment variable.

--- webapp/src/main/java/com/Data4Design/services/MapService.java
+++ webapp/src/main/java/com/Data4Design/services/MapService.java
@@ -13,42 +13,33 @@ import org.json.simple.parser.JSONParser;
 
 import com.Data4Design.results.LongResult;
 
-public class MapService implements IMapService{
+public class MapService implements IMapService {
 
-	
 	public StringResult getMap(String countryCode) {
-	       CountryListService countryList = new CountryListService();
-		   String countryName = countryList.getCountryName(countryCode);
-		   String apiKey = "AIzaSyCsMooQ7FQpjCIOHZ2bEiX61z1DFV6UBhI";//new api key generated. Expires after 12 months.
+		CountryListService countryList = new CountryListService();
+		String apiKey = System.getenv("GoogleMapsAPI");// new api key generated. Expires after 12 months.
+		String baseUrl = "https://maps.googleapis.com/maps/api/js?key=";
+		baseUrl += apiKey;
+		baseUrl += "&callback=initMap";
 
-	       String baseUrl = "https://maps.googleapis.com/maps/api/staticmap?center=";
-	       baseUrl+=countryName;
-	       baseUrl +="&zoom=6";
-	       baseUrl +="&size=800x400";
-	       baseUrl += "&key=";
-	       baseUrl +=apiKey;
-	       
-	       try {
-	    	       URL url = new URL(baseUrl);
-	           HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-	           connection.setRequestMethod("GET");
-	           
-	    	   
-	       }
-	       
-	       catch(Exception e){
-	    	   System.out.print("Something went wrong");
-	    	   
-	       }
-	       StringResult mapSource = new StringResult();
-	       
-	       System.out.println(baseUrl);
-	        
-	        mapSource.setData(baseUrl);
+		try {
+			URL url = new URL(baseUrl);
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
 
-	        return mapSource;
-	    }
-		
-		
-	
+		}
+
+		catch (Exception e) {
+			System.out.print("Something went wrong");
+
+		}
+		StringResult mapSource = new StringResult();
+
+		System.out.println(baseUrl);
+
+		mapSource.setData(baseUrl);
+
+		return mapSource;
+	}
+
 }
--- webapp/src/main/java/com/Data4Design/webapp/CountryLookupController.java
+++ webapp/src/main/java/com/Data4Design/webapp/CountryLookupController.java
@@ -1,7 +1,7 @@
 package com.Data4Design.webapp;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;  
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,22 +14,21 @@ import com.Data4Design.services.*;
 import com.Data4Design.results.*;
 
 @Controller
-@RequestMapping(value="/country")
+@RequestMapping(value = "/country")
 public class CountryLookupController {
 
     // inject via application.properties
-    //@Value("${index.message:test}")
-    //private String message = "Hello World";
+    // @Value("${index.message:test}")
+    // private String message = "Hello World";
 
-
-    @RequestMapping(value="/{id}", method = RequestMethod.GET)
-    //@ResponseBody
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    // @ResponseBody
     public String country_page(Map<String, Object> model, @PathVariable String id) {
-        //String Cheese if you please
+        // String Cheese if you please
         String str_id = String.valueOf(id);
         MapService mapService = new MapService();
-        
-        //Me Getters YO Ho Yo HO
+
+        // Me Getters YO Ho Yo HO
         ElectricityUsageService electricity_service = new ElectricityUsageService();
         NumberResult electricity_usage = electricity_service.getElectricityUsage(str_id);
         CountryPopulationService population_service = new CountryPopulationService();
@@ -43,13 +42,13 @@ public class CountryLookupController {
         String iso_3_str = country_list_service.toISO3(str_id);
         String cellpen = String.format("%d", cell_penetration.getCellPenetration(str_id).getData());
         String natural_resources = naturalResourceService.getNaturalResources(str_id).getData();
-        //Me Setters Yee Har Yee Har
+        // Me Setters Yee Har Yee Har
         String rainfall = String.format("%f", yearlyRainService.getAnnualPrecipitation(iso_3_str).getData());
-        String temperature = String.format("%f",yearlyTempService.getAverageAnnualTemperature(iso_3_str).getData());
+        String temperature = String.format("%f", yearlyTempService.getAverageAnnualTemperature(iso_3_str).getData());
         String country_list = country_list_service.getCountryListString();
         model.put("yearly_rainfall", rainfall);
-        model.put("yearly_temperature",temperature);
-        model.put("map",mapService.getMap(str_id).getData());
+        model.put("yearly_temperature", temperature);
+        model.put("baseUrl", mapService.getMap(str_id).getData());
         model.put("electricity_usage", electricity_usage.toString());
         model.put("population", population.toString());
         model.put("cell_penetration", cellpen);
@@ -58,12 +57,9 @@ public class CountryLookupController {
         model.put("country_name", country_name);
         model.put("natural_resources", natural_resources);
 
-        
-        
-
         model.put("country_list", country_list);
-        
-        //This chooses the jsp to grab
+
+        // This chooses the jsp to grab
         return "country_page";
     }
 
