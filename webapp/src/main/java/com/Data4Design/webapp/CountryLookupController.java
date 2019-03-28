3cf60766e441335d2e221bf8d2699d5eb5b81ec7
John McCarty <jhm822@utulsa.edu>
2019-03-27 20:38:15 -0500

    Changed the values being displayed on the webapp from yearly to monthly

--- webapp/src/main/java/com/Data4Design/webapp/CountryLookupController.java
+++ webapp/src/main/java/com/Data4Design/webapp/CountryLookupController.java
@@ -1,7 +1,7 @@
 package com.Data4Design.webapp;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;  
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,65 +27,65 @@ public class CountryLookupController {
     // inject via application.properties
     //@Value("${index.message:test}")
     //private String message = "Hello World";
-	
+
 	private Collection<ICountryInfoItemService> iCountryInfoItemServices = new ArrayList<ICountryInfoItemService>();
 	private IGetCountryInfoWorkflow countryInfoWorkflow;
-	
+
 
 
     @RequestMapping(value="/{id}", method = RequestMethod.GET)
     //@ResponseBody
     public String country_page(Map<String, Object> model, @PathVariable String id) {
-    	
+
     	/*CountryInfo countryInfo = new CountryInfo();
     	String str_id = String.valueOf(id);
     	CountryListService country_list_service = new CountryListService();
     	String country_name = country_list_service.getCountryName(str_id);
         String iso_3_str = country_list_service.toISO3(str_id);
-    	
-    	iCountryInfoItemServices.add(new AnnualPrecipitationServiceCountryInfoItem());
-    	iCountryInfoItemServices.add(new AnnualTemperatureServiceCountryInfoItem());
+
+    	iCountryInfoItemServices.add(new MonthlyPrecipitationServiceCountryInfoItem());
+    	iCountryInfoItemServices.add(new MonthlyTemperatureServiceCountryInfoItem());
     	iCountryInfoItemServices.add(new CellPenetrationServiceCountryInfoItem());
     	iCountryInfoItemServices.add(new CountryPopulationServiceCountryInfoItem());
     	iCountryInfoItemServices.add(new ElectricityUsageServiceCountryInfoItem());
     	iCountryInfoItemServices.add(new MapServiceCountryInfoItem());
     	iCountryInfoItemServices.add(new NaturalResourcesServiceCountryInfoItem());
-    	
+
     	try {
 			countryInfoWorkflow = new GetCountryInfoWorkflow(iCountryInfoItemServices);
 			countryInfo = countryInfoWorkflow.GetCountryInfo(str_id);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-    	
+
     	for(CountryInfoItem c: countryInfo.CountryInfoItems) {
     		model.put(c.Title, c.Value);
     	}
     	*/
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
+
+
+
+
+
+
+
+
+
+
+
+
         //String Cheese if you please
         String str_id = String.valueOf(id);
         MapService mapService = new MapService();
-        
+
         //Me Getters YO Ho Yo HO
         ElectricityUsageService electricity_service = new ElectricityUsageService();
         NumberResult electricity_usage = electricity_service.getElectricityUsage(str_id);
         CountryPopulationService population_service = new CountryPopulationService();
         CellPenetrationService cell_penetration = new CellPenetrationService();
-        AnnualPrecipitationServiceCountryInfoItem yearlyRainService = new AnnualPrecipitationServiceCountryInfoItem();
-        //AnnualTemperatureService yearlyTempService = new AnnualTemperatureService();
-        ICountryInfoItemService yearlyTempService = new AnnualTemperatureServiceCountryInfoItem();
+        MonthlyPrecipitationServiceCountryInfoItem monthlyRainService = new MonthlyPrecipitationServiceCountryInfoItem();
+        //MonthlyTemperatureService monthlyTempService = new MonthlyTemperatureService();
+        ICountryInfoItemService monthlyTempService = new MonthlyTemperatureServiceCountryInfoItem();
         LongResult population = population_service.getPopulation(str_id);
         CountryListService country_list_service = new CountryListService();
         NaturalResourceService naturalResourceService = new NaturalResourceService();
@@ -94,11 +94,11 @@ public class CountryLookupController {
         String cellpen = String.format("%d", cell_penetration.getCellPenetration(str_id).getData());
         String natural_resources = naturalResourceService.getNaturalResources(str_id).getData();
         //Me Setters Yee Har Yee Har
-        String rainfall = yearlyRainService.GetCountryInfoItem(iso_3_str).getValue();
-        String temperature = yearlyTempService.GetCountryInfoItem(iso_3_str).Value;
+        String rainfall = monthlyRainService.GetCountryInfoItem(iso_3_str).getValue();
+        String temperature = monthlyTempService.GetCountryInfoItem(iso_3_str).Value;
         String country_list = country_list_service.getCountryListString();
-        model.put("yearly_rainfall", rainfall);
-        model.put("yearly_temperature",temperature);
+        model.put("monthly_rainfall", rainfall);
+        model.put("monthly_temperature",temperature);
         model.put("map",mapService.getMap(str_id).getData());
         model.put("electricity_usage", electricity_usage.toString());
         model.put("population", population.toString());
@@ -108,11 +108,11 @@ public class CountryLookupController {
         model.put("country_name", country_name);
         model.put("natural_resources", natural_resources);
 
-        
-        
+
+
 
         model.put("country_list", country_list);
-        
+
         //This chooses the jsp to grab
         return "country_page";
     }
--- webapp/src/main/webapp/WEB-INF/jsp/country_page.jsp
+++ webapp/src/main/webapp/WEB-INF/jsp/country_page.jsp
@@ -19,7 +19,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    
+
     <script src="/js/node_modules/jquery/dist/jquery.min.js"></script>
     <script src="/js/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
     <!--<link href="/js/node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />-->
@@ -27,18 +27,18 @@
 
 
     <!-- Custom styles for this template -->
-    <link href="/css/startbootstrap-portfolio-item-gh-pages/css/portfolio-item.css" rel="stylesheet"> 
+    <link href="/css/startbootstrap-portfolio-item-gh-pages/css/portfolio-item.css" rel="stylesheet">
 
     <!-- Bootswatch for color -->
     <link href="/js/node_modules/bootswatch/dist/yeti/bootstrap.min.css" rel="stylesheet">
 
     <!-- Font Awesome Glyphs -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    
+
     <title>HHCD - ${country_name}</title>
 
-    
-    
+
+
   </head>
   <body class="">
     <!-- Navigation -->
@@ -102,8 +102,8 @@
           <p>${country_description}</p> -->
           <h3 class="my-3">Supplementary Details</h3>
           <ul>
-            <li><i class="fas fa-thermometer-half"></i> Average yearly temperature: ${yearly_temperature} mm</li>
-            <li><i class="fas fa-tint"></i> Average yearly rainfall: ${yearly_rainfall} mm</li>
+            <li><i class="fas fa-thermometer-half"></i> Average monthly temperature: ${monthly_temperature} mm</li>
+            <li><i class="fas fa-tint"></i> Average monthly rainfall: ${monthly_rainfall} mm</li>
             <li>ISO 2 Code: ${country_code}</li>
             <li>ISO 3 Code: ${country_code_3}</li>
           </ul>
@@ -138,8 +138,8 @@
        <div class="col-md-3 col-sm-6 mb-4">
             <div id ="electricity-usage-card" class="card" >
               <div class="card-body">
-                <h5 class="card-title"><i class="fas fa-tint"></i> Average Yearly Rainfall</h5>
-                <p class="card-text">${yearly_rainfall} mm</p>
+                <h5 class="card-title"><i class="fas fa-tint"></i> Average Monthly Rainfall</h5>
+                <p class="card-text">${monthly_rainfall} mm</p>
               </div>
             </div>
         </div>
@@ -160,10 +160,10 @@
       <div class="row">
 
         <div class="col-md-3 col-sm-6 mb-4">
-            <div id ="yearly-temperature-card" class="card" >
+            <div id ="monthly-temperature-card" class="card" >
               <div class="card-body">
-                <h5 class="card-title"><i class="fas fa-thermometer-half"></i> Average Yearly Temperature </h5>
-                <p class="card-text">${yearly_temperature}&#8451;</p>
+                <h5 class="card-title"><i class="fas fa-thermometer-half"></i> Average Monthly Temperature </h5>
+                <p class="card-text">${monthly_temperature}&#8451;</p>
               </div>
             </div>
         </div>
@@ -186,36 +186,36 @@
 
 <script>
     $(document).ready(function () {
-  
+
       // //One of these worked not sure which
       // $('#header_video').volume = 0;
       // document.getElementsByTagName('video')[0].volume = 0;
       // $("video").prop("volume", 0);
-  
+
       //console.log($('#header_video').volume);
-  
+
       //Get Countries
       var countryArray = JSON.parse('${country_list}');
       console.log(countryArray);
-  
+
       var sortable = [];
       $.each(countryArray, function (key, value) {
         sortable.push({ key, value });
       });
-  
+
       sortable.sort(function (a, b) {
         return (a.value > b.value) ? 1 : -1;
       });
-  
+
       for (var i = 0; i < sortable.length; i++) {
         key = sortable[i].key;
         value = sortable[i].value;
         $('#country_select').append("<option value=\"" + key + "\"><a href=\"/country/" + key + "\">" + value + "</a></option>");
       }
-  
+
     });
-  
-  
+
+
     function redirect(sel) {
       console.log($('#country_select').val());
       var selection = $('#country_select').val();
