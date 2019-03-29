45df18e561b3bae8a1f3262e1f5fdd80b42f1dd3
Jeff Meyer <jeffmeyer@Jeffs-iMac.local>
2019-03-29 11:39:05 -0500

    resolved conflicts

--- webapp/src/main/java/com/Data4Design/Implementations/MapServiceCountryInfoItem.java
+++ webapp/src/main/java/com/Data4Design/Implementations/MapServiceCountryInfoItem.java
@@ -11,28 +11,6 @@ import com.Data4Design.Workflows.Implementations.CountryInfoItem;
 public class MapServiceCountryInfoItem implements ICountryInfoItemService {
 
 	@Override
-<<<<<<< HEAD
-	public CountryInfoItem GetCountryInfoItem(String countryName) {
-		CountryListService countryList = new CountryListService();
-		   String CountryName = countryList.getCountryName(countryName);
-		   String oldApiKey = "AIzaSyBwhOcDvZryXjm3ZSUxHHEHeRQflJH5ctc";
-
-	       String baseUrl = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCkQwa9T2l2h0txTwEjunyYBS-KLLOX17k&callback=initMap";
-	       
-	    //    try {
-	    // 	       URL url = new URL(baseUrl);
-	    //        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-	    //        connection.setRequestMethod("GET");
-	           
-	    	   
-	    //    }
-	       
-	    //    catch(Exception e){
-	    // 	   System.out.print("Something went wrong");
-	    	   
-	    //    }
-	       
-=======
 	public CountryInfoItem GetCountryInfoItem(Country thisCountry) {
 		String apiKey = System.getenv("GoogleMapsAPI");// new api key generated. Expires after 12 months.
 		String baseUrl = "https://maps.googleapis.com/maps/api/js?key=";
@@ -50,7 +28,6 @@ public class MapServiceCountryInfoItem implements ICountryInfoItemService {
 			System.out.print("Something went wrong");
 
 		}
->>>>>>> 53a66afd9c5cc984ec6290e75c2e07dd847321b8
 	        
 	    CountryInfoItem countryInfoItem = new CountryInfoItem();
 	    countryInfoItem.setTitle("map");
